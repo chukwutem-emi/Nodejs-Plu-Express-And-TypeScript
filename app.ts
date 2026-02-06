8119075ac826ce505504b87ade38664c89cdb616
chukwutem-emi <chukwutememi@gmail.com>
2026-02-06 13:35:46 +0100

    updated

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import express, {type Request, type Response, type NextFunction} from "express";
-import todoRoutes from "./routes/todo.ts"
+import todoRoutes from "./routes/todo.js"
 
 const app = express();
 
